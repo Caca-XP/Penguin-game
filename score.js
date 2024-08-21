f73f55324abda089f32989bbfaa7b71c26d1984f
Natasha <126932965+Caca-XP@users.noreply.github.com>
2024-08-21 21:23:31 +1200

    Structure of score.js

---
+++ score.js
@@ -0,0 +1,47 @@
+export default class Score {
+
+
+    
+    /**
+     * Constructor
+     * initialize the score
+     * @param {CanvasRenderingContext2D} ctx
+     * @param {number} scaleRatio
+     */
+
+    constructor(ctx, scaleRatio){
+        // initialize the score properties
+    }
+
+    /**
+     * Method to handle the update of the score for each frame
+     * @param {number} frameTimeDelta
+     */
+    update(frameTimeDelta){
+
+    }
+
+    /**
+     * Method to reset the score
+     */
+    reset(){
+        // reset the score
+    }
+
+    /**
+     * Method to set high score
+     */
+    setHighScore(){
+        // check from local storage if the high score is set
+
+        // set the high score if current score is higher
+    }
+
+    /**
+     * Method to draw the score
+     */
+    draw(){
+        // draw the score
+    }
+
+}
