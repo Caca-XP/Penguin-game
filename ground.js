b73296261a67b425f25471330bd8738c813feb0e
Natasha <126932965+Caca-XP@users.noreply.github.com>
2024-08-21 21:19:11 +1200

    stucture of ground.js

---
+++ ground.js
@@ -0,0 +1,47 @@
+export default class Ground {
+
+    /**
+     * Constructor
+     * initialize the ground and its properties (position, speed, image)
+     * @param {CanvasRenderingContext2D} ctx
+     * @param {number} width
+     * @param {number} height
+     * @param {number} speed
+     * @param {number} scaleRatio
+     */
+    constructor(ctx, width, height, speed, scaleRatio){        
+        // initialize the ground properties
+
+        // ground position
+
+        // ground image
+    }
+
+    /**
+     * Method to handle the update of the ground image for each frame
+     * @param {number} gameSpeed
+     * @param {number} frameTimeDelta
+     */
+    update(gameSpeed, frameTimeDelta){
+        // ground update position
+    }
+
+    /**
+     * Method to draw the ground image
+     */
+    draw(){
+        // draw the ground image
+
+        // draw the ground image again to fill the gap
+
+        // if the ground is at the end of the screen, go back to the start position
+    }
+
+    /**
+     * Method to reset the ground position
+     */
+    reset(){
+        // reset the ground position
+    }
+
+}
