2318d6e4f53662bcda4641e14ec27cde55d2d8d6
Natasha <126932965+Caca-XP@users.noreply.github.com>
2024-08-22 22:45:32 +1200

    Player initialised

--- player.js
+++ player.js
@@ -3,6 +3,20 @@
  */
 
 export default class Player {
+    ANIMATION_TIMER = 200;
+    animationTimer = this.ANIMATION_TIMER;
+
+    // player variables
+    standingStillImage = null;
+    swimmingImages = [];
+    northEastImage = null;
+    southEastImage = null;
+
+    falling = false;
+    jumpInProgress = false;
+    jumpPressed = false;
+    JUMP_SPEED = 0.6;
+    GRAVITY = 0.4;
 
     /**
      * Constructor
@@ -16,15 +30,71 @@ export default class Player {
      */
     constructor(ctx, width, height, minJumpHeight, maxJumpHeight, scaleRatio){
         // initialize the player properties
+        this.ctx = ctx;
+        this.canvas = ctx.canvas;
+        this.width = width;
+        this.height = height;
+        this.minJumpHeight = minJumpHeight;
+        this.maxJumpHeight = maxJumpHeight;
+        this.scaleRatio = scaleRatio;
 
         // player position
+        this.x = this.width * this.scaleRatio + 10 * this.scaleRatio;
+        this.y = this.canvas.height/2 - this.height * this.scaleRatio;
 
         // player image
+        this.standingStillImage = new Image(); 
+        this.standingStillImage.src = './images/peng-still.png';
+        this.image = this.standingStillImage;
 
-        // remove and add event listeners
+        const swim1 = new Image();
+        swim1.src = './images/peng-swim1.png';
+        const swim2 = new Image();
+        swim2.src = './images/peng-E.png';
+        const swim3 = new Image();
+        swim3.src = './images/peng-swim2.png';
+
+        this.swimmingImages.push(swim1);
+        this.swimmingImages.push(swim2);
+        this.swimmingImages.push(swim3);
+        
+        this.northEastImage = new Image();
+        this.northEastImage.src = './images/peng-NE.png';
+        this.southEastImage = new Image();
+        this.southEastImage.src = './images/peng-SE.png';
+        
+        // remove if exists and add event listeners
+        window.removeEventListener('touchstart', this.touchstart);
+        window.removeEventListener('touchend', this.touchend);
+        window.removeEventListener('keydown', this.keydown);
+        window.removeEventListener('keyup', this.keyup);
+
+        window.addEventListener('touchstart', this.touchstart);
+        window.addEventListener('touchend', this.touchend);
+        window.addEventListener('keydown', this.keydown);
+        window.addEventListener('keyup', this.keyup);
     }
 
     // event listeners, touchstart, touchend, keydown, keyup 
+    touchstart = () => {
+        this.jumpPressed = true;
+    }
+
+    touchend = () => {
+        this.jumpPressed = false;
+    }
+
+    keydown = (event) => {
+        if (event.code ==='Space' || event.code === 'ArrowUp'){
+            this.jumpPressed = true;
+        }
+    }
+
+    keyup = (event) => {
+        if (event.code ==='Space' || event.code === 'ArrowUp'){
+            this.jumpPressed = false;
+        }
+    }
 
     /**
      * Method to handle the update of the player image for each frame
@@ -33,8 +103,10 @@ export default class Player {
      */
     update(gameSpeed, frameTimeDelta){
         // player running update
+        this.run(gameSpeed, frameTimeDelta);
 
         // player jumping update
+        this.jump(frameTimeDelta);
 
     }
 
@@ -44,11 +116,37 @@ export default class Player {
      */
     jump(frameTimeDelta){
         // player jumping logic
+        if(this.jumpPressed){
+            this.jumpInProgress = true;
+        }
 
         // when in progress and not falling
+        if (this.jumpInProgress && !this.falling){
+            // this means the player is increasing in height
+            // when the player reaches the min jump height or the max jump height, the player will start falling
+            if (this.y < this.minJumpHeight || (this.y < this.maxJumpHeight && this.jumpInProgress)){
+                this.y -= this.JUMP_SPEED * frameTimeDelta * this.scaleRatio;
+                this.image = this.northEastImage;
+            } else {
+                this.falling = true;
+            }
+
+        }
 
 
         // when in progress and falling
+        if (this.jumpInProgress && this.falling){
+            // this means the player is falling
+            // when the player reaches the ground, the player will stop falling
+            if (this.y < this.canvas.height/2 - this.height * this.scaleRatio){
+                this.y += this.GRAVITY * frameTimeDelta * this.scaleRatio;
+                this.image = this.southEastImage;
+            } else {
+                this.y = this.canvas.height/2 - this.height * this.scaleRatio;
+                this.jumpInProgress = false;
+                this.falling = false;
+            }
+        }
 
 
         // end the progress
@@ -61,8 +159,14 @@ export default class Player {
      */
     run(gameSpeed, frameTimeDelta){
         // player running logic
-
         // alterate the player image for running
+        this.animationTimer -= frameTimeDelta * gameSpeed;
+        if (this.animationTimer <= 0){
+            this.animationTimer = this.ANIMATION_TIMER;
+            this.image = this.swimmingImages[0];
+            this.swimmingImages.push(this.swimmingImages.shift());
+        }
+
     }
 
 
@@ -71,5 +175,6 @@ export default class Player {
      */
     draw(){
         // draw the player on the ctx
+        this.ctx.drawImage(this.image, this.x, this.y, this.width * this.scaleRatio, this.height * this.scaleRatio);
     }
 }
